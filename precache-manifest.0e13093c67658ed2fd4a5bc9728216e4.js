17dfb7f0c1045927520a14eda138e826ba9ac21f
“Milo <milogirardet@gmail.com>
2020-07-02 10:32:39 +0100

    Updates

---
+++ precache-manifest.0e13093c67658ed2fd4a5bc9728216e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa25b81890d806b58333046d527f89d1",
+    "url": "/react-spacex-api/index.html"
+  },
+  {
+    "revision": "5e731a429ac60de3736b",
+    "url": "/react-spacex-api/static/css/main.33aa3cfc.chunk.css"
+  },
+  {
+    "revision": "85d1afd4baefef6fdc49",
+    "url": "/react-spacex-api/static/js/2.fb4eba29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-spacex-api/static/js/2.fb4eba29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e731a429ac60de3736b",
+    "url": "/react-spacex-api/static/js/main.da995c1b.chunk.js"
+  },
+  {
+    "revision": "ded0f8bbc3dea92b4b25",
+    "url": "/react-spacex-api/static/js/runtime-main.70dd1e7b.js"
+  },
+  {
+    "revision": "f2b1b847235119d0ae094a606c74fca4",
+    "url": "/react-spacex-api/static/media/spacex.f2b1b847.jpg"
+  }
+]);
